6442e9ab6564b77959331f8a7dd92225640cde85
Connor Ferster <connorferster@gmail.com>
2025-07-18 10:24:43 -0700

    Add tests, indent in dump_json

--- src/jsonchain/__init__.py
+++ src/jsonchain/__init__.py
@@ -1,3 +1,10 @@
+"""
+A small package to aid in the use of the chaining techniques taught by Structural Python
+"""
+
+__version__ = "0.1.0"
+
+
 import json
 import pathlib
 from typing import Any, Optional
@@ -10,13 +17,13 @@ def load_json(filepath: str | pathlib.Path) -> dict | list:
         return json.load(file)
     
 
-def dump_json(object: list | dict, filepath: str | pathlib.Path) -> None:
+def dump_json(object: list | dict, filepath: str | pathlib.Path, indent=2) -> None:
     """
     Dumps the 'object' (which must be JSON-serializable) into a JSON
     file at 'filepath'.
     """
     with open(filepath, 'w') as file:
-        json.dump(object, file)
+        json.dump(object, file, indent=indent)
 
 
 def extract_keys(
@@ -56,5 +63,3 @@ def extract_keys(
             acc.append({key_name: key})
 
     return acc
-
-
