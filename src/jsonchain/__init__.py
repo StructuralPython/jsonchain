2004cbc9f3f0c73efda9b2e3eabc533ae27bf50b
Connor Ferster <connorferster@gmail.com>
2025-08-26 10:37:40 -0700

    bump version

--- src/jsonchain/__init__.py
+++ src/jsonchain/__init__.py
@@ -2,7 +2,7 @@
 A small package to aid in the use of the chaining techniques taught by Structural Python
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 
 from .io import (load_json, dump_json)
